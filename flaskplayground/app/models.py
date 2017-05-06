c946972cefb1d26d8a7091304f5ca962148429ee
Jackson Burnette <jackson.d.burnette@intel.com>
2017-05-05 18:49:17 -0700

    Incomplete work on DB models, using db_scripts/create_db.py as template

--- flaskplayground/app/models.py
+++ flaskplayground/app/models.py
@@ -19,4 +19,59 @@ class Child(db.Model):
     is_active = db.Column(db.Boolean)
 
     def __repr__(self):
-        return '<%r; %r>' % (self.nickname, self.id)
+        return '<Child %r>' % (self.nickname)
+
+
+class Partner(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    english_name = db.Column(db.String(255))
+    chinese_name = db.Column(db.String(255))
+    pinyin_name = db.Column(db.String(255))
+    email = db.Column(db.String(255))
+    address = db.Column(db.String(255))
+    phone = db.Column(db.String(255))
+
+    def __repr__(self):
+        return '<Partner %r>' % (self.english_name)
+
+
+class Specialist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    english_name = db.Column(db.String(255))
+    chinese_name = db.Column(db.String(255))
+    pinyin_name = db.Column(db.String(255))
+    assessment_specialty = db.Column(db.String(255))
+
+    def __repr__(self):
+        return '<Specialist %r>' % (self.english_name)
+
+
+class AssessmentType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    english_name = db.Column(db.String(255))
+    chinese_name = db.Column(db.String(255))
+    pinyin_name = db.Column(db.String(255))
+
+    def __repr__(self):
+        return '<Assessment Type %r>' % (self.english_name)
+
+
+class MilestoneTypeCategory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    english_name = db.Column(db.String(255))
+    chinese_name = db.Column(db.String(255))
+    pinyin_name = db.Column(db.String(255))
+
+    def __repr__(self):
+        return '<Milestone Type Category %r>' % (self.english_name)
+
+
+class MilestoneType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    english_name = db.Column(db.String(255))
+    chinese_name = db.Column(db.String(255))
+    pinyin_name = db.Column(db.String(255))
+    milestone_category = db.Column(db.String(16))                                  # ???????????????
+
+    def __repr__(self):
+        return '<Milestone Type Category %r>' % (self.english_name)
