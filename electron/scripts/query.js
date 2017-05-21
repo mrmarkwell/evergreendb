56961ea069e4773ce7531fbd905cee792ec44cdb
Richard Clermont <rc2524@users.noreply.github.com>
2017-05-20 20:58:11 -0700

    started child query page

--- electron/scripts/query.js
+++ electron/scripts/query.js
@@ -1,22 +1,3 @@
-function restRun(method,url) {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open( method, url, false ); // false for synchronous request
-  xmlHttp.send( null );
-  return xmlHttp.responseText;
-}
-
-function restGet(url) {
-  return JSON.parse(restRun("GET", url));
-}
-
-function restPost(url,data) { // Create
-
-}
-
-function restPut(url,data) { // Update
-
-}
-
 function getAllChildren(callback) {
   var data = null;
 
@@ -33,4 +14,24 @@ function getAllChildren(callback) {
   xhr.setRequestHeader("cache-control", "no-cache");
   xhr.setRequestHeader("postman-token", "a34d63fa-7642-c59d-b236-8437ff0d2068");
   xhr.send(data);
+}
+
+function jsonTable(table_id, json, columns) {
+  // header row
+  var table = "<tr>";
+  for (var i=0; i<columns.length; i++) {
+    table += "<th>" + columns[i] + "</th>";
+  }
+  table += "</tr>";
+
+  // data rows
+  for (var i=0; i<json.length; i++) {
+    table += "<tr>";
+    for (var j=0; j<columns.length; j++) {
+      table += "<td>" + json[i][columns[j]];
+    }
+  }
+
+  // write to table
+  document.getElementById(table_id).innerHTML = table;
 }
