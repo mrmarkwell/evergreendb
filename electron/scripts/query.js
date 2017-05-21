7bbdae093b26b9b3153030f12af9d264c423c46d
Richard Clermont <rc2524@users.noreply.github.com>
2017-05-20 21:27:32 -0700

    added table css so we can easily keep look consistant, also updates to query page

--- electron/scripts/query.js
+++ electron/scripts/query.js
@@ -1,22 +1,18 @@
 function getAllChildren(callback) {
-  var data = null;
-
   var xhr = new XMLHttpRequest();
   xhr.withCredentials = true;
 
   xhr.addEventListener("readystatechange", function () {
     if (this.readyState === 4) {
       console.log(this.responseText);
-      callback(JSON.parse(this.responseText),"child_table",["english_name"]);
+      callback(JSON.parse(this.responseText));
     }
   });
   xhr.open("GET", "http://127.0.0.1:5000/entity/child");
-  xhr.setRequestHeader("cache-control", "no-cache");
-  xhr.setRequestHeader("postman-token", "a34d63fa-7642-c59d-b236-8437ff0d2068");
-  xhr.send(data);
+  xhr.send(null);
 }
 
-function jsonTable(table_id, json, columns) {
+function jsonTable(json, table_ref, columns) {
   // header row
   var table = "<tr>";
   for (var i=0; i<columns.length; i++) {
@@ -33,5 +29,5 @@ function jsonTable(table_id, json, columns) {
   }
 
   // write to table
-  document.getElementById(table_id).innerHTML = table;
+  table_ref.innerHTML = table;
 }
