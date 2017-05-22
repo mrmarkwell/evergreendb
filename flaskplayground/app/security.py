558cfaaf0c11160f62e3ce82fc37689d3c32326d
tcase <traviscase@gmail.com>
2017-05-21 22:35:12 -0700

    adding the security file defines our crypto usage

---
+++ flaskplayground/app/security.py
@@ -0,0 +1,17 @@
+from passlib.context import CryptContext
+
+ = CryptContext(
+    # Replace this list with the hash(es) you wish to support.
+    # this example sets pbkdf2_sha256 as the default,
+    schemes=["pbkdf2_sha256"],
+
+    # Automatically mark all but first hasher in list as deprecated.
+    # (this will be the default in Passlib 2.0)
+    deprecated="auto",
+
+    # Optionally, set the number of rounds that should be used.
+    # Appropriate values may vary for different schemes,
+    # and the amount of time you wish it to take.
+    # Leaving this alone is usually safe, and will use passlib's defaults.
+    ## pbkdf2_sha256__rounds = 29000,
+    )
