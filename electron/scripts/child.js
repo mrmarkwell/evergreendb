01fab58f6dc4122f627787a065238d308a891be7
Richard Clermont <rc2524@users.noreply.github.com>
2017-05-21 18:23:40 -0700

    added rest to child tab of child page

--- electron/scripts/child.js
+++ electron/scripts/child.js
@@ -1,16 +1,55 @@
 function getTabData(entity, child_id) {
-	var data = null;
-
+	var BASE_URL = "http://127.0.0.1:5000/";
 	var xhr = new XMLHttpRequest();
 	xhr.withCredentials = true;
-
 	xhr.addEventListener("readystatechange", function () {
 		if (this.readyState === 4) {
-			console.log(this.responseText);
+			fillChildTabData(JSON.parse(this.responseText));
 		}
 	});
+	var query = jQuery.param({"id":child_id});
+	var url = BASE_URL + "entity/" + entity + "?" + query;
+	xhr.open("GET", url);
+	xhr.send(null);
+}
 
-	xhr.open("GET", "http://127.0.0.1:5000/entity/child?id=2");
+function fillChildTabData(json) {
+	// Make sure only one child is returned and get that child from array
+	if (json.length != 1) {
+		console.log("Expected one child to be returned got ",json.length);
+		return;
+	}
+	child_data = json[0];
 
-	xhr.send(data);
+	fillNormalFields(child_data); // Fills text fields that match name of column
+
+	// Fill special fields
+	// age
+	fillAge(child_data.birth_date)
+	// gender
+	if (child_data.sex == "M") {
+		document.getElementById("sex_m").checked = true;
+	} else if (child_data.sex = "F") {
+		document.getElementById("sex_f").checked = true;
+	} 
+}
+
+function fillNormalFields(jsonDict) {
+	for (var key in jsonDict) {
+		var obj = document.getElementById(key);
+		if (obj != null) {
+			obj.value = jsonDict[key];
+		}
+	}
+}
+
+function fillAge(birthdate_str) {
+	var today = new Date();
+	var birthdate = jQuery.datepicker.parseDate("yy-mm-dd",birthdate_str);
+	var age = today.getFullYear() - birthdate.getFullYear();
+	var m = today.getMonth() - birthdate.getMonth();
+	if (m < 0 || (m === 0 && today.getDate() < birthdate.getDate())) {
+		age--;
+	}
+	document.getElementById('age').innerHTML = age;
 }
