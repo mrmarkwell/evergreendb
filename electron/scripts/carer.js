74294ba98ec95bef8df876c799f8c48f5e705cc3
Jackson Burnette <jacksondburnette@gmail.com>
2017-06-01 12:06:04 -0700

    [CARER_TABLE_GEN] move makeRequest to lib.js, add responseType

--- electron/scripts/carer.js
+++ electron/scripts/carer.js
@@ -12,55 +12,6 @@ function d_log(record) {
     }
 }
 
-// Function to handle asynchronous HTTP requests with promises.
-/* Takes opts:
-{
-  method: String,
-  url: String,
-  params: String | Object,
-  headers: Object
-}
-*/
-function makeRequest(opts) {
-    return new Promise(function (resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.open(opts.method, opts.url);
-        xhr.onload = function () {
-            if (this.status >= 200 && this.status < 300) {
-                resolve(xhr.response);
-            } else {
-                reject({
-                    status: this.status,
-                    statusText: xhr.statusText
-                });
-            }
-        };
-        xhr.onerror = function () {
-            reject({
-                status: this.status,
-                statusText: xhr.statusText
-            });
-        };
-        if (opts.headers) {
-            Object.keys(opts.headers).forEach(function (key) {
-                xhr.setRequestHeader(key, opts.headers[key]);
-            });
-        }
-        if (opts.responseType) {
-            xhr.responseType = opts.responseType;
-        }
-        var params = opts.params;
-        // We'll need to stringify if we've been given an object
-        // If we have a string, this is skipped.
-        if (params && typeof params === 'object') {
-            params = Object.keys(params).map(function (key) {
-                return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
-            }).join('&');
-        }
-        xhr.send(params);
-    });
-}
-
 function TableElements() {
     return {
         caregiver_id: null,
