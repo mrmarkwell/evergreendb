356ee2d0258d2fdf909ca0ce7b607f26261f6c75
Jackson Burnette <jacksondburnette@gmail.com>
2017-06-05 05:35:24 -0700

    [CARER_UPDATE] replace checkbox to end child/caregiver relationship

--- electron/scripts/carer.js
+++ electron/scripts/carer.js
@@ -1,6 +1,7 @@
 const settings = require('electron-settings');
 let carer = {
-    base_url: settings.get('url')
+    base_url: settings.get('url'),
+    child_id: null
 }
 
 // log to console if in debug mode
@@ -41,6 +42,7 @@ function populateCarerTableElements(jdata) {
             }
         }
         te.href = "child.html?id=" + record.child_id
+        te.checkboxFuncKey = "endChildCaregiverRelationship"
         if (curr_caregiver) {
             curr_tes.push(te)
             curr_caregiver = false;
@@ -51,33 +53,64 @@ function populateCarerTableElements(jdata) {
     return [hist_tes, curr_tes];
 }
 
+function endChildCaregiverRelationship(element, idx, row_obj) {
+    if (!element.checked) {
+        alert("This checkbox only supports ENDING a child/caregiver relationship (cannot uncheck)")
+        element.checked = true;
+    }
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1; //January is 0
+    let yyyy = today.getFullYear();
+    if(dd < 10) { dd = '0' + dd; }
+    if(mm < 10) { mm = '0' + mm; }
+    today = yyyy + '-' + mm + '-' + dd;
+    makeRequest({
+        method: "POST",
+        url: carer.base_url + "entity/child_caregiver",
+        headers: {"Content-Type": "application/json"},
+        responseType: "json",
+        params: JSON.stringify({
+            "child_caregiver_end_date": today,
+            "child_caregiver_start_date": row_obj.child_caregiver_start_date,
+            "child_caregiver_note": row_obj.child_caregiver_note,
+            "child_id": carer.child_id,
+            "caregiver_id": row_obj.caregiver_id,
+        })
+    });
+}
+
 function constructCarerTable(table_entries, table_id) {
     let headers = [
-        "",          // edit link
+        "", "Ended",          // edit link and checkbox respectively
         "English Name", "Chinese Name",
         "Pinyin Name", "Start Date",
         "End Date", "Note"
     ];
     let columnTypes = [
-        columnTypeEnum.editFormLink,
+        columnTypeEnum.editFormLink, columnTypeEnum.checkboxFunc,
         columnTypeEnum.text, columnTypeEnum.text,
         columnTypeEnum.text, columnTypeEnum.text,
         columnTypeEnum.text, columnTypeEnum.text
     ];
     let fieldNames = [
-        "href",
+        "href", "checkboxFuncKey",
         "caregiver_english_name", "caregiver_chinese_name",
         "caregiver_pinyin_name", "child_caregiver_start_date",
         "child_caregiver_end_date", "child_caregiver_note"
     ];
+    let checkedDecision = function(row_obj) {
+        return row_obj.child_caregiver_end_date != null;
+    }
     let columnData = new ColumnData(headers, columnTypes, fieldNames);
-    let tdata = new TableData(table_id, columnData, table_entries);
+    let tdata = new TableData(table_id, columnData, table_entries, checkedDecision);
     generateTable(tdata);
 }
 
 // Main entry point. Do all the initial REST calls and get all the data.
 // Construct the table.
 function initializeCarerTable(child_id) {
+    carer.child_id = child_id;
     const caregiver_url = carer.base_url + "entity/child,child_caregiver,caregiver?child_id=" + child_id
 
     makeRequest({
