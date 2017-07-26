974766bc58471ad9542cfb63457ea5faef982d80
Richard Clermont <rc2524@users.noreply.github.com>
2017-07-25 21:32:44 -0700

    initial angular fss app creation

---
+++ fss/src/app/children/children-tab.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'children-tab',
+	template: `
+	 <h1>The Children tab placeholder</h1>
+	 <h3>add a child-list component</h3>
+	 <h3>and a child-info component</h3>
+	 <h3>And the child-tab component</h3>
+	`
+})
+export class ChildrenTab {}
