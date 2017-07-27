c28e9ff7fd72b400e61259ace8e7d5a6401bf1d9
Richard Clermont <rc2524@users.noreply.github.com>
2017-07-26 18:51:06 -0700

    Fixed menu bar router links

--- fss/src/app/app-routing.module.ts
+++ fss/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ChildrenTab } from './children/children-tab.component';
 import { RemindersTab } from './reminders/reminders-tab.component';
+import { CalendarTab } from './calendar/calendar-tab.component';
 
 const routes : Routes = [
-	{ path: '', redirectTo:'reminders', pathMatch:'full' },
+	{ path: '', redirectTo:'/reminders', pathMatch:'full' },
 	{ path: 'children', component: ChildrenTab },
 	{ path: 'reminders', component: RemindersTab },
-	{ path: 'calendar', component: ChildrenTab },
+	{ path: 'calendar', component: CalendarTab },
 ]
 
 @NgModule({
