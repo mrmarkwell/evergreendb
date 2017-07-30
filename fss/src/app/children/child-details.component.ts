a51b836080a825c52abb706efda849d0555a033c
Richard Clermont <rc2524@users.noreply.github.com>
2017-07-29 23:16:45 -0700

    Added base layout for children tab

---
+++ fss/src/app/children/child-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'child-details',
+	template: `
+	 <h1>Child Details</h1>
+	 <h3>with basic info</h3>
+	`
+})
+export class ChildDetails {}
