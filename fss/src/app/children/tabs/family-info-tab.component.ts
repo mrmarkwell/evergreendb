58dd037f8c00a1b336dccf6b8b735ac077194107
Jackson Burnette <jackson.d.burnette@intel.com>
2017-08-12 14:42:00 -0700

    [CHILD TABS] first pass at tabs

---
+++ fss/src/app/children/tabs/family-info-tab.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'family-info-tab',
+	templateUrl: './family-info-tab.component.html'
+})
+export class FamilyTabComponent {}
---
+++ fss/src/app/children/tabs/interactions-tab.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'interactions-tab',
+	templateUrl: './interactions-tab.component.html'
+})
+export class InteractionsTabComponent {}
