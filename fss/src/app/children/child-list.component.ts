08d3c1b072545fd0867f0b3781da85caa0d98981
tcase <traviscase@gmail.com>
2017-08-15 21:21:23 -0700

    Added search capability to child list

--- fss/src/app/children/child-list.component.ts
+++ fss/src/app/children/child-list.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import { Child } from './child';
 
 @Component({
 	selector: 'child-list',
 	template: `
-	 <h3>search bar</h3>
-	 <p>child1</p>
-	 <p>child2</p>
-	 <p>child3</p>
+		<h3>type in bar to search by name</h3>
+		<input (keyup)="onKey($event)">
+		<p>{{values}}</p>
+	  <ul class="children">
+	    <li *ngFor="let child of filteredChildren"
+		    [class.selected]="child === selectedChild"
+		    (click)="onSelect(child)">
+		    <span class="id">{{child.id}}</span> {{child.name}}
+		  </li>
+	  </ul>				
+	`,
+
+	styles: [`
+	   .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
 	`
+	]
 })
-export class ChildList {}
+export class ChildList {
+
+	allChildren: Child[] = [
+		{ id: 1, name: "Bill" },
+		{ id: 2, name: "Bob" },
+		{ id: 3, name: "Lilly" },
+		{ id: 4, name: "Eden" },
+		{ id: 5, name: "Nora" },
+	]
+
+	filteredChildren: Child[] = this.allChildren;
+
+	selectedChild: Child;
+
+	onSelect(child: Child): void {
+		this.selectedChild = child
+	}
+
+	onKey(event: any) {
+		this.filteredChildren = this.allChildren.filter(Child => Child.name.toLowerCase().indexOf(event.target.value.toLowerCase()) !== -1)
+	}
+}
---
+++ fss/src/app/children/child.ts
@@ -0,0 +1,4 @@
+export class Child {
+  id: number;
+  name: string;
+}
