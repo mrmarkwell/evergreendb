0f18c848c5b4197ad3d96104f749980a95d2bbb4
Richard Clermont <rc2524@users.noreply.github.com>
2017-08-18 22:43:07 -0700

    ++rest service functions. TODO: enum update, test put/post/del non-child

--- fss/src/app/rest.service.ts
+++ fss/src/app/rest.service.ts
@@ -3,37 +3,74 @@ import {Http, Headers} from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
 import { Child } from './child';
+import { Interaction } from './interaction';
+import { FamilyMember } from './family-member';
+import { ProjectedPathway } from './projected-pathway';
 
 @Injectable()
 export class RestService {
 	constructor(private http: Http) {}
 
-	// If we add cache to getChildren, it would reduce bandwidth.
-	getChildren(): Promise<Child[]> {
-		let childrenUrl = this.evergreenUrl + '/entity/fss_child';
-		return this.http.get(childrenUrl)
-			.toPromise().then(response => response.json() as Child[])
+	getEntity(type: string, query?:string): Promise<any> {
+		let url = `${this.evergreenUrl}/entity/${type}`;
+		if (query) {url = url + '?' + query};
+		return this.http.get(url)
+			.toPromise().then(response => response.json())
 			.catch(this.handleError);
 	}
-	getChild(id): Promise<Child> {
-		return this.getChildren().then(children => children.find(child => child.id === id))
-	}
-	addChild(child: Child): Promise<Child> {
-		let childrenUrl = this.evergreenUrl + '/entity/fss_child';
-		return this.http.post(childrenUrl, JSON.stringify(child), {headers: this.headers})
-			.toPromise().then(res => res.json().data as Child)
+	addEntity(type: string, entity: any): Promise<any> {
+		let url = `${this.evergreenUrl}/entity/${type}`;
+		return this.http.post(url, JSON.stringify(entity), {headers: this.headers})
+			.toPromise().then(res => res.json().data)
 			.catch(this.handleError);
 	}
-	updateChild(child: Child): Promise<Child> {
-		const childUrl = `${this.evergreenUrl}/entity/fss_child?id=${child.id}`;
-		console.log(JSON.stringify(child));
-		return this.http.put(childUrl, JSON.stringify(child), {headers: this.headers})
-			.toPromise().then(() => child).catch(this.handleError);
+	updateEntity(type:string, entity: any): Promise<any> {
+		const url = `${this.evergreenUrl}/entity/${type}?id=${entity.id}`;
+		return this.http.put(url, JSON.stringify(entity), {headers: this.headers})
+			.toPromise().then(res => res.json().data)
+			.catch(this.handleError);
 	}
-	deleteChild(id: number): Promise<void> {
-		const url = `${this.evergreenUrl}/entity/fss_child?id=${id}`;
+	deleteEntity(type: string, id: number): Promise<void> {
+		const url = `${this.evergreenUrl}/entity/${type}?id=${id}`;
 		return this.http.delete(url, {headers: this.headers})
 			.toPromise().then( () => null ).catch(this.handleError);
+	}
+
+	getEnum(field: string): Promise<string[]> {
+		let url = `${this.evergreenUrl}/enum/${field}`;
+		return this.http.get(url)
+			.toPromise().then(response => response.json())
+			.catch(this.handleError);
+	}
+
+	getInteractions(child_id: number): Promise<Interaction[]> {
+		return this.getEntity('fss_interaction',`child_id=${child_id}`).then( results => results as Interaction[] );
+	}
+	getFamilyMembers(child_id: number): Promise<FamilyMember[]> {
+		return this.getEntity('fss_family_member',`child_id=${child_id}`).then( results => results as FamilyMember[] );
+	}
+	getProjectedPathway(child_id: number): Promise<ProjectedPathway[]> {
+		return this.getEntity('fss_projected_pathway',`child_id=${child_id}`).then( results => results as ProjectedPathway[] );
+	}
+
+	getChildren(refresh: boolean = true): Promise<Child[]> {
+		if (refresh || !this.childrenCache) {
+			return this.getEntity('fss_child').then( results => this.childrenCache = results as Child[] );
+		} else {
+			return new Promise( (resolve,reject) => resolve(this.childrenCache) );
+		}
+	}
+	getChild(child_id: number): Promise<Child> {
+		return this.getChildren(false).then(children => children.find(child => child.id === child_id))
+	}
+	addChild(child: Child): Promise<Child> {
+		return this.addEntity('fss_child', child).then(results => results as Child);
+	}
+	updateChild(child: Child): Promise<Child> {
+		return this.updateEntity('fss_child', child).then(results => results as Child);
+	}
+	deleteChild(id: number): Promise<void> {
+		return this.deleteEntity('fss_child',id);
 	}
 
 	private handleError(error: any): Promise<any> {
@@ -43,4 +80,5 @@ export class RestService {
 
 	private evergreenUrl = 'http://127.0.0.1:5000';
 	private headers = new Headers({'Content-Type': 'application/json'});
+	private childrenCache: Child[];
 }
