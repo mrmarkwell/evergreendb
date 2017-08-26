4cc5b1059243de32b3b7cfe7462ff841d9751064
Jackson Burnette <jackson.d.burnette@intel.com>
2017-08-26 12:15:34 -0700

    [FAMILY INFO TAB] use md list for sidebar - BROKE

--- fss/src/app/children/tabs/family-info-tab.component.ts
+++ fss/src/app/children/tabs/family-info-tab.component.ts
@@ -1,7 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ParamMap } from '@angular/router';
+
+import { FamilyMember } from '../../family-member';
+import { RestService } from '../../rest.service';
 
 @Component({
-	selector: 'family-info-tab',
-	templateUrl: './family-info-tab.component.html'
+    selector: 'family-info-tab',
+    templateUrl: './family-info-tab.component.html'
 })
-export class FamilyTabComponent {}
+export class FamilyTabComponent implements OnInit {
+    constructor(
+        private restService: RestService
+    ) {}
+    ngOnInit(): void {
+        this.restService.getFamilyMembers(1).then(family_members => { console.log(family_members); this.family_members =  family_members });
+    }
+    private family_members: FamilyMember[];
+}
