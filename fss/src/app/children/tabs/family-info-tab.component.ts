75c206243fb59286afd097532e08661cd8c32e54
Clermont, Richard <richard.clermont@intel.com>
2017-08-26 14:51:30 -0700

    Add family member tab details from selected in list

--- fss/src/app/children/tabs/family-info-tab.component.ts
+++ fss/src/app/children/tabs/family-info-tab.component.ts
@@ -6,7 +6,8 @@ import { RestService } from '../../rest.service';
 
 @Component({
     selector: 'family-info-tab',
-    templateUrl: './family-info-tab.component.html'
+    templateUrl: './family-info-tab.component.html',
+    styleUrls: ['./family-info-tab.component.css']
 })
 export class FamilyTabComponent implements OnInit {
     constructor(
@@ -14,4 +15,9 @@ export class FamilyTabComponent implements OnInit {
     ) {}
     ngOnInit(): void {
     }
+		onSelect(family_member: FamilyMember): void {
+		  this.family_member = family_member;
+			console.log("yay",this.family_member);
+		}
+    private family_member: FamilyMember;
 }
