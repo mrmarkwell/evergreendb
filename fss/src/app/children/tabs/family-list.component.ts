b07e3499e541b06c6289096090784a02ccbf8dea
Richard Clermont <rc2524@users.noreply.github.com>
2017-08-26 20:40:52 -0700

    Add save, delete (w/confirm). fixed selected css on new.

--- fss/src/app/children/tabs/family-list.component.ts
+++ fss/src/app/children/tabs/family-list.component.ts
@@ -13,30 +13,32 @@ export class FamilyListComponent implements OnInit {
     constructor(
         private restService: RestService
     ) {}
-    @Input() child_id: number;
     ngOnInit(): void {
         this.getFamilyMembers();
+    }
+    isSelected(family_member): boolean {
+      // Object equality doesn't work in js/ts. It just checks if instances are the same not contents are equal
+      return this.selected_family_member && family_member.id === this.selected_family_member.id;
     }
     onSelect(selected: FamilyMember) : void {
       this.selected_family_member = selected;
       this.notifySelected.emit(selected);
     }
-    private family_members: FamilyMember[];
-    private selected_family_member: FamilyMember;
-    @Output() notifySelected = new EventEmitter<FamilyMember>();
     createFamilyMember() : void {
         let family_member = new FamilyMember();
         family_member.relationship = "Family Member";
         family_member.child_id = this.child_id;
         this.restService.addFamilyMember(family_member).then(new_family_member => {
-            console.log(new_family_member)
             this.getFamilyMembers();
             this.onSelect(new_family_member);
         });
     }
     getFamilyMembers(): void {
-        this.restService.getFamilyMembers(this.child_id).then(family_members => {
-            console.log(family_members); this.family_members =  family_members
-        });
+        this.restService.getFamilyMembers(this.child_id)
+            .then(family_members => this.family_members = family_members );
     }
+    @Input() child_id: number;
+    @Output() notifySelected = new EventEmitter<FamilyMember>();
+    private family_members: FamilyMember[];
+    private selected_family_member: FamilyMember;
 }
