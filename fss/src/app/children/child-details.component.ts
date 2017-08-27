b3b566029cf4ca4b7be2ea3b70644af26308111b
Richard Clermont <rc2524@users.noreply.github.com>
2017-08-27 16:57:54 -0700

    diagnosis drop down, child delete w/data in other tables, child_id input

--- fss/src/app/children/child-details.component.ts
+++ fss/src/app/children/child-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 import { ParamMap } from '@angular/router';
 import { DatePipe } from '@angular/common';
 
@@ -10,31 +10,53 @@ import { RestService } from '../rest.service';
 	templateUrl: "./child-details.component.html",
 	styleUrls: ["./child-details.component.css"]
 })
-export class ChildDetails implements OnInit {
+export class ChildDetails implements OnInit, OnChanges {
 	constructor(
 		private restService: RestService,
 		private datePipe: DatePipe
 	) {}
 	ngOnInit(): void {
-		this.restService.getChild(1).then(child => {this.child = child; this.age = child.getAge()});
+		this.restService.getEnum("fss_medical_condition").then(conditions => this.medical_conditions = conditions);
+	}
+	ngOnChanges(): void {
+		console.log(this.child_id);
+		this.restService.getChild(this.child_id).then(child => {this.child = child; this.age = child.getAge()});
 	}
 	saveChild(): void {
 		this.restService.updateChild(this.child);
 	}
 	deleteChild(): void {
-		//this.restService.deleteChild(this.child.id); // Add warning
-		console.log(this.child.birth_date);
+		if (confirm("Are you sure you want to delete this child and all associated data?")) {
+			this.restService.deleteChild(this.child.id);
+			//TODO: Delete associated family members, pathways, and interactions
+			this.restService.getFamilyMembers(this.child.id).then(fam_mems => {
+				for (let member of fam_mems) {
+					this.restService.deleteFamilyMember(member.id);
+				}
+			});
+			this.restService.getInteractions(this.child.id).then(interactions => {
+				for (let interaction of interactions) {
+					this.restService.deleteFamilyMember(interaction.id);
+				}
+			});
+			this.restService.getProjectedPathway(this.child.id).then(pathways => {
+				for (let pathway of pathways) {
+					this.restService.deleteFamilyMember(pathway.id);
+				}
+			});
+			this.child = null;
+		}
 	}
 
 	setDoB(date): void {
-		// Two problems. 1) date not displayed in right format causing date to be displayed wrong because of timezone
-		//               2) Age doesn't seem to be right around birthday, might have to do more complicated method based on string
 		console.log(date);
 		this.child.birth_date = this.datePipe.transform(date, 'yyyy-MM-dd');
 		console.log(this.child.birth_date);
 		this.age = this.child.getAge();
 		console.log(this.age);
 	}
+	@Input() child_id: number;
 	private child: Child;
 	private age: number;
+	private medical_conditions: string[];
 }
