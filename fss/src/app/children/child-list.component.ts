70b5aaf660db1b15bb7674ccf7459a4c05ccc9b7
Matthew Markwell <matthewmarkwell@gmail.com>
2017-09-23 15:03:23 -0700

    [NEW_CHILD_BUTTON] Finished a new child button.

--- fss/src/app/children/child-list.component.ts
+++ fss/src/app/children/child-list.component.ts
@@ -16,15 +16,24 @@ export class ChildList implements OnInit, OnChanges {
 
     ngOnInit(): void {
         this.getChildren();
+        this.restService.changeEmitter.subscribe(() => this.ngOnChanges())
     }
     
     ngOnChanges(): void {
         this.getChildren()
-        this.restService.changeEmitter.subscribe(() => this.ngOnChanges())
     }
     
-    constructor(private restService: RestService) {
+    constructor(private restService: RestService) {}
 
+    createNewChild(): void {
+        let new_child = new Child(null);
+        new_child.child_pinyin_name = "New Child";
+        this.restService.addChild(new_child).then(the_child => this.onSelect(the_child));
+    }
+
+    isSelected(child): boolean {
+      // Object equality doesn't work in js/ts. It just checks if instances are the same not contents are equal
+      return this.selectedChild && child.id === this.selectedChild.id;
     }
 
     onSelect(child: Child): void {
