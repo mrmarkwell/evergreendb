2e255adb81bdb54e6b8dd6efbc0fbc4bf3c77053
Matthew Markwell <matthewmarkwell@gmail.com>
2017-09-23 15:42:59 -0700

    [REFRESH] Added a refresh button. Also made all the icons md-icon-buttons so they react to being clicked.

--- fss/src/app/app.component.ts
+++ fss/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { RestService } from './rest.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MdIconRegistry } from '@angular/material';
 
 @Component({
-  selector: 'fss-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'fss-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FSS';
+    title = 'FSS';
+
+    constructor(
+        iconRegistry: MdIconRegistry,
+        sanitizer: DomSanitizer,
+        private restService: RestService
+    ) {
+        iconRegistry.addSvgIcon(
+            'refresh_icon',
+            sanitizer.bypassSecurityTrustResourceUrl('assets/refresh_icon.svg'));
+    }
+
+    refresh(): void {
+        this.restService.changeEmitter.emit();
+    }
 }
