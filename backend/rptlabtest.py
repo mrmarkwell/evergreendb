58b132097f0303b8360b38d4c7d4e45ff0e00924
Clermont, Richard <richard.clermont@intel.com>
2017-12-09 12:17:53 -0800

    Change to gen from string and confirm works in windows

--- backend/rptlabtest.py
+++ backend/rptlabtest.py
@@ -2,5 +2,7 @@
 import pdfkit
 
 # Requires: sudo apt-get install wkhtmltopdf
-
-pdfkit.from_file('/home/richard/tmp/test.html', '/home/richard/tmp/example.pdf', options={'quiet':''})
+#   Or for Windows installing https://wkhtmltopdf.org/downloads.html (MinGW worked for me) and adding to path
+html_str = '<html><h1>Testing is the Best thing</h1></html>'
+dest = 'C:\\Users\\rclermon\\Documents\\Docs\\Other\\test.pdf'
+pdfkit.from_string(html_str, dest, options={'quiet':''})
