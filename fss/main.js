82d014110d47bcd540dc1adc27b14973195715be
Matthew Markwell <matthewmarkwell@gmail.com>
2017-12-08 23:17:39 -0800

    [COPY PASTE] Added edit menu which gets copy-paste working.

--- fss/main.js
+++ fss/main.js
@@ -1,15 +1,37 @@
 // ./main.js
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu} = require('electron')
 const path = require('path');
 const url = require('url');
 
 let win = null;
 
+var template = [{
+    label: "Application",
+    submenu: [
+        { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+        { type: "separator" },
+        { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+    ]}, {
+    label: "Edit",
+    submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]}
+];
+  
+  const menu = Menu.buildFromTemplate(template)
+
 app.on('ready', function () {
 
 	// Initialize the window to our specified dimensions
 	win = new BrowserWindow({width: 1000, height: 600});
-
+    Menu.setApplicationMenu(menu)
+    
 	// Specify entry point
 	win.loadURL(url.format({
 		pathname: path.join(__dirname, 'dist/index.html'),
