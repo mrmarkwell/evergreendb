379110a454fe8d839c4e6afaee906bcd139af9ce
Jackson Burnette <jackson.d.burnette@intel.com>
2017-12-09 17:14:41 -0800

    add common form

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,10 +1,12 @@
+from glob import glob
+import os
 from os.path import join
 
 from flask import render_template, send_from_directory
 from app import app, db
 from models import FSSChild
 from config import basedir
-#import pdfkit
+import pdfkit
 
 @app.route('/forms/<form_type>/<child_id>')
 def generate_forms(form_type, child_id):
@@ -14,10 +16,12 @@ def generate_forms(form_type, child_id):
 
     sesh = db.session
     child = sesh.query(FSSChild).filter_by(id=child_id).first()
+    child_photo = glob(join('app', 'static', 'photos', '*%d.*' % child.id))[0].split(os.sep)[-1]
 
-    return render_template('form_common.html', title=form_type, child=child)
-
-    rpt_html = render_template('form_common.html', title=form_type, child=child)
+    rpt_html = render_template('form_common.html',
+                               title=form_type,
+                               child=child,
+                               child_photo=child_photo)
 
     pdfkit.from_string(rpt_html, rpt_path, options={'quiet':''})
     return send_from_directory(temp_dir, rpt_name, mimetype='application/pdf')
