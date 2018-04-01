816fcffad56cdef60bc925ed7ce4d30cabc767ea
root <root@ip-172-31-14-180.us-west-1.compute.internal>
2018-04-01 00:53:54 +0000

    Added production backend run command as a script to the repo.

---
+++ backend/run_prod_backend.sh
@@ -0,0 +1,1 @@
+killall gunicorn; rm ~/logs/*; cd ~/evergreendb/backend/; /home/apps/evergreendb/backend/venv/bin/gunicorn app:app -b localhost:8000 --error-log ~apps/logs/error.log --access-logfile ~apps/logs/evergreen.log &
