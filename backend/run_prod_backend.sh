641956c23020608076a044be559fbe2666621e94
Matthew Markwell <matthewmarkwell@gmail.com>
2018-03-31 18:03:43 -0700

    updated production backend run scripts

--- backend/run_prod_backend.sh
+++ backend/run_prod_backend.sh
@@ -1,1 +1,1 @@
-killall gunicorn; rm ~/logs/*; cd ~/evergreendb/backend/; /home/apps/evergreendb/backend/venv/bin/gunicorn app:app -b localhost:8000 --error-log ~apps/logs/error.log --access-logfile ~apps/logs/evergreen.log &
+mkdir -p ~/backup_logs; mkdir -p ~/logs; mv ~/logs/* ~/backup_logs/;  cd ~/evergreendb/backend/; /home/apps/evergreendb/backend/venv/bin/gunicorn app:app -b localhost:8000 --error-log ~apps/logs/error.log --access-logfile ~apps/logs/evergreen.log &
