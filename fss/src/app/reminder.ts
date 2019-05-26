884a04033e89ac28454c1fd21160274da38774d9
Matthew Markwell <matthewmarkwell@gmail.com>
2019-05-25 20:27:51 -0700

    Reminders include projected pathways.

--- fss/src/app/reminder.ts
+++ fss/src/app/reminder.ts
@@ -1,11 +1,34 @@
 import { Interaction } from 'app/interaction';
+import { ProjectedPathway } from 'app/projected-pathway';
+import * as moment from 'moment';
 
 export class Reminder {
-    constructor(interaction){
-		this.interaction = interaction
+    static fromInteraction(interaction) {
+        return new this(
+            interaction.child_id,
+            interaction.interaction_date,
+            interaction.interaction_type,
+            interaction.interaction_notes
+        )
     }
-    
-    interaction: Interaction;
+
+    static fromProjectedPathway(projected_pathway) {
+        return new this(
+            projected_pathway.child_id,
+            projected_pathway.pathway_completion_date,
+            "Projected Pathway",
+            projected_pathway.pathway_short_description
+        )
+    }
+
+    constructor(
+        public child_id: number,
+        public date: string,
+        public type: string,
+        public notes: string,
+    ) {}
+
+    date_object: moment.Moment;
     child_chinese_name: string;
     child_pinyin_name: string;
 }
