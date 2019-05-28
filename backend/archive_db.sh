cef18ffae96d75266efcbfcb6dd156eb24b1c6c5
Richard Clermont <rc2524@users.noreply.github.com>
2019-05-27 21:04:08 -0700

    Add archiving files/photos to archive script

--- backend/archive_db.sh
+++ backend/archive_db.sh
@@ -1,7 +1,20 @@
+date=`date +%b_%d_%Y_%H_%M_%S`
 mkdir -p db_archives
 if [ -f fss.db ]; then
-    mv fss.db db_archives/fss_$(date +%b_%d_%Y_%H_%M_%S).db
+    mv fss.db "db_archives/fss_$date.db"
 fi
 if [ -f soar.db ]; then
-    mv soar.db db_archives/soar_$(date +%b_%d_%Y_%H_%M_%S).db
+    mv soar.db "db_archives/soar_$date.db"
+fi
+if [ -d app/static/photos ]; then
+    mkdir -p db_archives/static_$date
+    mv app/static/photos "db_archives/static_$date/photos"
+fi
+if [ -d app/static/medical ]; then
+    mkdir -p db_archives/static_$date
+    mv app/static/medical "db_archives/static_$date/medical"
+fi
+if [ -d app/static/interactions ]; then
+    mkdir -p db_archives/static_$date
+    mv app/static/interactions "db_archives/static_$date/interactions"
 fi
