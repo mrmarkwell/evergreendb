792dcba165e2435f34b5acdcada181b619b9cdc9
Richard Clermont <rc2524@users.noreply.github.com>
2019-06-01 19:55:25 -0700

    Reminders redo to use backend instead of front end.

--- fss/src/app/reminder.ts
+++ fss/src/app/reminder.ts
@@ -1,34 +1,16 @@
-import { Interaction } from 'app/interaction';
-import { ProjectedPathway } from 'app/projected-pathway';
 import * as moment from 'moment';
 
 export class Reminder {
-    static fromInteraction(interaction) {
-        return new this(
-            interaction.child_id,
-            interaction.interaction_date,
-            interaction.interaction_type,
-            interaction.interaction_notes
-        )
-    }
-
-    static fromProjectedPathway(projected_pathway) {
-        return new this(
-            projected_pathway.child_id,
-            projected_pathway.pathway_completion_date,
-            "Projected Pathway",
-            projected_pathway.pathway_short_description
-        )
-    }
 
     constructor(
         public child_id: number,
+        public child_pinyin_name: string,
+        public child_chinese_name: string,
+        public child_nickname: string,
         public date: string,
         public type: string,
         public notes: string,
     ) {}
 
     date_object: moment.Moment;
-    child_chinese_name: string;
-    child_pinyin_name: string;
 }
--- fss/src/app/reminders/reminder-list.component.ts
+++ fss/src/app/reminders/reminder-list.component.ts
@@ -13,7 +13,6 @@ import { Reminder } from 'app/reminder';
 
 export class ReminderList implements OnInit, OnChanges {
     allReminders: Reminder[];
-    filteredReminders: Reminder[];
 
     ngOnInit(): void {
         this.restService.changeEmitter.subscribe(() => this.ngOnChanges())
@@ -28,30 +27,11 @@ export class ReminderList implements OnInit, OnChanges {
 
     getReminders(): void {
         this.restService.getAllReminders().then(reminders => {
-            reminders.forEach(reminder => {
-                this.restService.getChild(reminder.child_id).then(child => {
-                    reminder.child_chinese_name = child.child_chinese_name;
-                    reminder.child_pinyin_name = child.child_pinyin_name;
-                });
-            });
-            
             // populate date object
             for (let reminder of reminders) {
                 reminder.date_object = this.restService.getDateFromString(reminder.date)
             }
-            // sort the list by date object
-            this.allReminders = reminders.sort((n1,n2) => {
-                if (n1.date_object < n2.date_object) {
-                    return -1;
-                }
-                if (n1.date_object > n2.date_object) {
-                    return 1;
-                }
-                return 0;
-            });
-
-            let d = moment();
-            this.filteredReminders = this.allReminders.filter(reminder => reminder.date_object > d)
+            this.allReminders = reminders;
         });
     }
 }
